d2092da38d3430be471f19bc0a0db2e40dffde4b
DaleAnn <daviddaleann@yahoo.com>
2023-03-10 01:59:13 +0000

    Done!

--- src/main/java/ReturnLongestString.java
+++ src/main/java/ReturnLongestString.java
@@ -8,6 +8,16 @@ public class ReturnLongestString {
      * @return the longest String within arr. You can assume that there are no ties for the longest string.
      */
     public String longest(String[] arr){
-        return null;
+        int num = 0;
+        String s = "null";
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i].length() > num){
+                num = arr[i].length();
+                s = arr[i];
+
+            }
+         }
+        return s;
     }
 }
+
